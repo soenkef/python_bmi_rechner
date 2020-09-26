a9ae9d002380d84e6523b06f8f13d31a640007ba
soenkef <mantu@gmx.de>
2020-09-26 10:37:01 +0200

    BMI-Rechner mit Komma-Eingabe-Möglichkeit gebaut

--- programm.py
+++ programm.py
@@ -3,6 +3,6 @@ print("BMI-Rechner")
 weight = input("Bitte gebe dein Gewicht in KG wie z.B. 84.2 kg ein: ")
 height = input("Bitte gebe deine Körpergröße im Meter wie z.B. 1.82 ein: ")
 
-result = (float(weight) / (float(height) * float(height)))
+result = (float(weight.replace(",", ".")) / (float(height.replace(",", ".")) ** 2))
 
-print("Dein BMI liegt bei: " + str(result))
+print("Dein BMI liegt bei: " + str(round(result, 1)))
