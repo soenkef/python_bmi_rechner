1c4e4fa3d90c0a51ccbf88f6a1d3680557d57589
soenkef <mantu@gmx.de>
2020-09-27 17:06:46 +0200

    dateien nach x durchsuchen - filelist angepasst

--- names.py
+++ names.py
@@ -5,19 +5,20 @@ import os
 print("\nHier gehts los:")
 folder = os.path.join(os.path.dirname(__file__), "names")
 print(folder)
-print(os.listdir("names"))
-filelist = os.listdir("names")
+#print(os.listdir("names"))
+filelist = os.listdir(folder)
+#print(filelist)
 
 counter = 0
 
 for item in filelist:
     filename = os.path.join(folder, item)
-    print(filename)
+    #print(filename)
     #print(folder)
     with open(filename, "r", encoding="utf-8") as file:
         for line in file:
             seperated_line = line.split()
-            print(seperated_line)
+            #print(seperated_line)
             if seperated_line[0] == "Max":
                 counter = counter + 1
 
