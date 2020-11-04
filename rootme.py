b031b8e2a986d2f74804a731c6f0417e109c7ea7
soenkef <mantu@gmx.de>
2020-11-04 19:36:32 +0100

    root-me.org login und andere api calls probiert

--- rootme.py
+++ rootme.py
@@ -7,23 +7,24 @@ class Login():
     def __init__(self):
         payload = {'login': 'mantu@gmx.de', 'password': '57Y.swjtQ*2+LKN'}
         url = "https://api.www.root-me.org/login"
-        r = requests.get(url, params=payload)
-        cookie = r.headers['Set-Cookie'].split()
-        self.spip = cookie[0].strip(";")
-        self.spip_list = cookie[0].strip(";").split("=")
-        self.spip_value = cookie[0].strip(";").split("=")[1]
+        with requests.get(url, params=payload) as r:
+            cookie = r.headers['Set-Cookie'].split()
+            self.spip = cookie[0].strip(";")
+            self.spip_list = cookie[0].strip(";").split("=")
+            self.spip_value = cookie[0].strip(";").split("=")[1]
 
     def get_spip(self):
-        return self.spip
+        return self.spip_value
 
     def get_challenge(self, value):
-        cookies = {"spip_session": self.spip}
-        url = "https://api.root-me.org/challenges/ch" + str(value)
-        print(url)
-        r = requests.get(url, cookies=cookies)
-        print(r.headers)
+        spip = {"spip_session": str(self.get_spip())}
+        #url = "https://api.www.root-me.org/challenges/" + str(value)
+        url = "https://api.www.root-me.org/auteurs/" + str(value)
+        #print(url)
+        with requests.get(url, cookies=spip) as r:
+            print(r.content)
 
 
 obj = Login()
-print(obj.get_spip())
-obj.get_challenge(58)
+#print(obj.get_spip())
+obj.get_challenge(60)
